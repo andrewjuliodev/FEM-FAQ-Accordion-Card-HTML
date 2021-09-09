59b32076f1f7cd687c134942193d4fc50e44eb24
Andrew JT <andrew.julio.tompsett@gmail.com>
2021-09-09 21:08:24 +0200

    Fixed the JS Logic to only allow a single open answer at a time

--- js/script.js
+++ js/script.js
@@ -1,22 +1,13 @@
-const accordion = document.getElementsByClassName("container");
+const accordion = document.getElementsByTagName("details");
 
 for (let i = 1; i < accordion.length + 1; i++) {
-  document.querySelector(`.row${i}`).addEventListener("click", function () {
-    if (document.querySelector(`.cont${i}`).style.height === "55px") {
+  document.querySelector(`.q${i}`).addEventListener("click", function () {
+    if (document.querySelector(`.d${i}`).open === false) {
       for (let i = 1; i < accordion.length + 1; i++) {
-        if (document.querySelector(`.cont${i}`).style.height === "100px") {
-          document.querySelector(`.arrow${i}`).style.transform = "scaleY(1)";
-          document.querySelector(`.cont${i}`).style.height = "55px";
-          document.querySelector(`.a${i}`).style.display = "none";
+        if (document.querySelector(`.d${i}`).open === true) {
+          document.querySelector(`.d${i}`).open = false;
         }
       }
-      document.querySelector(`.arrow${i}`).style.transform = "scaleY(-1)";
-      document.querySelector(`.cont${i}`).style.height = "100px";
-      document.querySelector(`.a${i}`).style.display = "block";
-    } else {
-      document.querySelector(`.arrow${i}`).style.transform = "scaleY(1)";
-      document.querySelector(`.cont${i}`).style.height = "55px";
-      document.querySelector(`.a${i}`).style.display = "none";
     }
   });
 }
